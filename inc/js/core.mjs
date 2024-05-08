1ae84aeb5bb07800d9de471f62ac86cbf266db10
Erik Jespersen <42016062+Mookse@users.noreply.github.com>
2024-05-08 13:37:29 -0400

    Initialize 168-avatar-experience

---
+++ inc/js/core.mjs
@@ -0,0 +1,363 @@
+//	imports
+import EventEmitter from 'events'
+import chalk from 'chalk'
+import { _ } from 'ajv'
+//	server-specific imports
+import initRouter from './routes.mjs'
+//	define export Classes for Members and MyLife
+class Member extends EventEmitter {
+	#avatar
+	#factory
+	constructor(_Factory){
+		super()
+		this.#factory = _Factory // member will need factory after avatar is created for datacore evolutionary agent, et likely al.
+		/* assign factory/avatar listeners */
+		this.attachListeners()
+	}
+	/**
+	 * Initializes `this.#avatar` and returns `this`. The Avatar will thence be primary point of interaction with Session and Server. Only upon dissolution of Avatar [sessionEnd] is triggered an internal `this` evolution evaluation based on upon the conduct of Avatar.
+	 * @async
+	 * @public
+	 * @returns {Promise} Promise resolves to this Member class instantiation
+	 */
+	async init(){
+		this.#avatar = await this.factory.getAvatar()
+		// @todo: add consent and evolution agents for self-evolution from avatar data
+		return this
+	}
+	/**
+	 * Attaches listeners to Member class instantiation
+	 * @publc must remain public in order to be overridden
+	 * @returns {void} returns nothing
+	*/
+	attachListeners(){
+		this.factory.on('avatar-init-end',(_avatar,bytes)=>{
+			console.log(chalk.grey(`Member::init::avatar-init-end|memory-size=${bytes}b`))
+		})
+		this.factory.on('on-contribution-new',(_contribution)=>{
+			console.log(chalk.grey(`Member::on-contribution-new`),_contribution.request)
+		})
+	}
+	//	getter/setter functions
+	get abilities(){
+		return this.core.abilities
+	}
+	get agent(){
+		return this.#avatar
+	}
+	get agentCategories(){
+		return this.agent.categories
+	}
+	get agentCommand(){
+		return this.agent.command_word
+	}
+	get agentDescription(){	//	agent description (not required)
+		if(!this.agent?.description)
+			this.avatar.description = `I am ${ this.agentName }, AI-Agent for ${ this.name }`
+		return this.agent.description
+	}
+	get agentName(){
+		return this.avatar.names[0]
+	}
+	get agentProxy(){
+		switch(this.form){	//	need switch because I could not overload class function
+			case 'organization':
+				return this.description
+			default:
+				return this.bio
+		}
+	}
+	get avatar(){
+		return this.#avatar
+	}
+	set avatar(_Avatar){
+		//	oops, hack around how to get dna of avatar class; review options [could block at factory-getter level, most efficient and logical]
+		if(!this.factory.isAvatar(_Avatar))
+			throw new Error('avatar requires Avatar Class')
+		this.#avatar = _Avatar
+	}
+	/**
+	 * Gets being type. Members are _only_ human prototypes, and presents as human, even though the current manifestation is a datacore `core` being.
+	 * @returns {string} returns 'human'
+	*/
+	get being(){
+		return 'human'
+	}
+	get beliefs(){
+		return this.core.beliefs
+	}
+	get bio(){
+		return this.core.bio
+	}
+	get categories(){
+		return this.agent.categories
+	}
+	get chat(){
+		return this.agent.chat
+	}
+	get consent(){
+		return this.factory.consent	//	**caution**: returns <<PROMISE>>
+	}
+	set consent(_consent){
+		this.factory.consents.unshift(_consent.id)
+	}
+	/**
+	 * Gets Member Contributions, i.e., questions that need posing to Member
+	 * @returns {array} returns array of Member Contributions
+	 */
+	get contributions(){
+		return this.#avatar?.contributions
+	}
+	get core(){
+		return this.factory.core
+	}
+	get dataservice(){
+		return this.dataservices
+	}
+	get dataservices(){
+		return this.factory.dataservices
+	}
+	get description(){
+		return this.core.description
+	}
+	get email(){
+		return this.core.email
+	}
+	get factory(){
+		return this.#factory
+	}
+	get facts(){
+		return this.core.facts
+	}
+	get form(){
+		return this.core.form
+	}
+	get globals(){
+		return this.factory.globals
+	}
+	get hobbies(){
+		return this.core.hobbies
+	}
+	get interests(){
+		return this.core.interests
+	}
+	get id(){
+		return this.sysid
+	}
+	get mbr_id(){
+		return this.factory.mbr_id
+	}
+	get member(){
+		return this.core
+	}
+	get memberName(){
+		return this.core.names[0]
+	}
+	get name(){
+		return this.core.name
+	}
+	get newGuid(){
+		return this.globals.newGuid
+	}
+	get preferences(){
+		return this.core.preferences
+	}
+	get skills(){
+		return this.core.skills
+	}
+	get sysname(){
+		return this.mbr_id.split('|')[0]
+	}	
+	get sysid(){
+		return this.mbr_id.split('|')[1]
+	}
+	get values(){
+		return this.core.values
+	}
+	//	public functions
+	async testEmitters(){
+		//	test emitters with callbacks
+		this.emit('testEmitter',_response=>{
+			console.log('callback emitters enabled:',_response)
+		})
+	}
+}
+class Organization extends Member {	//	form=organization
+	#Menu
+	#Router
+	constructor(_Factory){
+		super(_Factory)
+	}
+	/* public functions */
+	async init(){
+		return await super.init()
+	}
+	/**
+	 * `Member` Overload
+	 * @publc must remain public in order to be overridden
+	 * @returns {void} returns nothing
+	*/
+	attachListeners(){
+		// intentionally empty
+	}
+	/* getters/setters */
+	/**
+	 * Gets being type. MyLife are _only_ MyLife prototypes, and presents as mylife, even though the current manifestation is a datacore `core` being.
+	 * @returns {string} returns 'MyLife'
+	*/
+	get being(){
+		return 'MyLife'
+	}
+	get description(){
+		return this.core.description
+	}
+	get governance(){
+		return this.core.governance
+	}
+	get membership(){
+		return this.core.membership
+	}
+	get menu(){
+		if(!this.#Menu){
+			this.#Menu = new (this.factory.schemas.menu)(this).menu
+		}
+		return this.#Menu
+	}
+	get mission(){
+		return this.core.mission
+	}
+	get name(){
+		return this.core.names[0]
+	}
+	get philosophy(){
+		return this.core.philosophy
+	}
+	get privacy(){
+		return this.core.privacy
+	}
+	get roadmap(){
+		return this.core.roadmap
+	}
+	get router(){
+		if(!this.#Router){
+			this.#Router = initRouter(new (this.factory.schemas.menu)(this))
+		}
+		return this.#Router
+	}
+	get security(){
+		return this.core.security
+	}
+	get services(){
+		return this.core.services
+	}
+	get values(){
+		return this.core.values
+	}
+	get vision(){
+		return this.core.vision
+	}
+}
+class MyLife extends Organization {	//	form=server
+	constructor(factory){	//	no session presumed to exist
+		super(factory)
+	}
+	async datacore(_mbr_id){
+		if(!_mbr_id || _mbr_id===this.mbr_id) throw new Error('datacore cannot be accessed')
+		return await this.factory.datacore(_mbr_id)
+	}
+	/* public functions */
+	/**
+	 * Server MyLife _Maht instantiation uses this function to populate the most current alerts in the modular factory memoryspace. Currently only applicable to system types, but since this is implemented at the `core.mjs` scope, we can account
+	 * @public
+	 * @returns {void} returns nothing, performs operation
+	 */
+	getAlerts(){
+		this.factory.getAlerts()
+	}
+	async getMyLifeSession(){
+		return await this.factory.getMyLifeSession()
+	}
+	/**
+	 * Submits a request for a library item from MyLife via API.
+	 * @public
+	 * @param {string} _mbr_id - Requesting Member id.
+	 * @param {string} _assistantType - String name of assistant type.
+	 * @param {string} _library - Library entry with or without `items`.
+	 * @returns {object} - The library document from Cosmos.
+	 */
+	async library(_mbr_id, _assistantType, _library){
+		_library.assistantType = _assistantType
+		_library.id = _library.id && this.globals.isValidGuid(_library.id) ? _library.id : this.globals.newGuid
+		_library.mbr_id = _mbr_id
+		_library.type = _library.type??_assistantType??'personal'
+		const _libraryCosmos = await this.factory.library(_library)
+		return this.globals.stripCosmosFields(_libraryCosmos)
+	}
+	/**
+	 * Registers a new candidate to MyLife membership
+	 * @public
+	 * @param {object} _candidate { 'email': string, 'humanName': string, 'avatarNickname': string }
+	 */
+	async registerCandidate(_candidate){
+		return await this.factory.registerCandidate(_candidate)
+	}
+	/**
+	 * Submits a story to MyLife via API. Unclear if can be dual-purposed for internal, or if internal still instantiates API context.
+	 * @public
+	 * @param {string} _mbr_id - Member id
+	 * @param {string} _assistantType - String name of assistant type
+	 * @param {string} _summary - String summary of story
+	 * @returns {object} - The story document from Cosmos.
+	 */
+	async story(_mbr_id, _assistantType, storySummary){
+		const id = this.globals.newGuid
+		const _story = {
+			assistantType: _assistantType,
+			being: 'story',
+			form: _assistantType,
+			id,
+			mbr_id: _mbr_id,
+			name: `story_${_assistantType}_${_mbr_id}`,
+			summary: storySummary,
+		}
+		const _storyCosmos = await this.factory.story(_story)
+		return this.globals.stripCosmosFields(_storyCosmos)
+	}
+	/**
+	 * Tests partition key for member
+	 * @public
+	 * @param {string} _mbr_id member id
+	 * @returns {boolean} returns true if partition key is valid
+	 */
+	async testPartitionKey(_mbr_id){
+		return await this.factory.testPartitionKey(_mbr_id)
+	}
+	/* getters/setters */
+	/**
+	 * Gets MyLife agent role, refers to server entity Maht/MyLife
+	 * @returns {object} returns { role: 'system', content: 'I am <||Q||>, AI-Agent for the nonprofit member organization MyLife' }
+	*/
+	get agentRole(){
+		switch(this.being){
+			default:	//	core
+				const replacedDescription = 'I am <||Q||>, AI-Agent for the nonprofit member organization MyLife'
+				return {
+						role: "system",
+						content: replacedDescription
+					}					
+		}
+	}
+	/**
+	 * Gets being type. MyLife are _only_ MyLife prototypes, and presents as mylife, even though the current manifestation is a datacore `core` being.
+	 * @returns {string} returns 'MyLife'
+	*/
+	get being(){
+		return 'MyLife'
+	}
+}
+/* exports */
+export {
+	Member,
+	Organization,
+	MyLife,
+}
---
+++ inc/js/routes.mjs
@@ -0,0 +1,143 @@
+// imports
+import Router from 'koa-router'
+import {
+    about,
+    activateBot,
+    alerts,
+    bots,
+    category,
+    challenge,
+    chat,
+    contributions,
+    index,
+    interfaceMode,
+    login,
+    loginSelect,
+    members,
+    privacyPolicy,
+    signup,
+    upload,
+    _upload
+} from './functions.mjs'
+import {
+    experience,
+    experienceCast,
+    experienceEnd,
+    experienceManifest,
+    experienceNavigation,
+    experiences,
+    keyValidation,
+    library,
+    login as apiLogin,
+    register,
+    story,
+    storyLibrary,
+    tokenValidation,
+} from './api-functions.mjs'
+// variables
+const _Router = new Router()
+const _memberRouter = new Router()
+const _apiRouter = new Router()
+//	root routes
+_Router.get('/', index)
+_Router.get('/about', about)
+_Router.get('/alerts', alerts)
+_Router.get('/login/:mid', login)
+_Router.get('/select', loginSelect)
+_Router.get('/status', status)
+_Router.get('/privacy-policy', privacyPolicy)
+_Router.get('/signup', status_signup)
+_Router.post('/', chat)
+_Router.post('/challenge/:mid', challenge)
+_Router.post('/signup', signup)
+/* api webhook routes */
+_apiRouter.use(tokenValidation)
+_apiRouter.get('/alerts', alerts)
+_apiRouter.get('/alerts/:aid', alerts)
+_apiRouter.get('/experiences/:mid', experiences) // **note**: currently triggers autoplay experience
+_apiRouter.get('/login/:mid', apiLogin)
+_apiRouter.head('/keyValidation/:mid', keyValidation)
+_apiRouter.patch('/experiences/:mid/experience/:eid/cast', experienceCast)
+_apiRouter.patch('/experiences/:mid/experience/:eid/end', experienceEnd)
+_apiRouter.patch('/experiences/:mid/experience/:eid/manifest', experienceManifest) // proxy for both cast and navigation
+_apiRouter.patch('/experiences/:mid/experience/:eid/navigation', experienceNavigation)
+_apiRouter.patch('/experiences/:mid/experience/:eid', experience) // **note**: This line should be the last one alphabetically due to the wildcard.
+_apiRouter.post('/challenge/:mid', challenge)
+_apiRouter.post('/keyValidation/:mid', keyValidation)
+_apiRouter.post('/library/:mid', library)
+_apiRouter.post('/register', register)
+_apiRouter.post('/story/library/:mid', storyLibrary) /* ordered first for path rendering */
+_apiRouter.post('/story/:mid', story)
+/* member routes */
+_memberRouter.use(memberValidation)
+_memberRouter.get('/', members)
+_memberRouter.get('/bots', bots)
+_memberRouter.get('/bots/:bid', bots)
+_memberRouter.get('/contributions', contributions)
+_memberRouter.get('/contributions/:cid', contributions)
+_memberRouter.get('/experiences', experiences)
+_memberRouter.get('/mode', interfaceMode)
+_memberRouter.get('/select', loginSelect)
+_memberRouter.get('/upload', upload)
+_memberRouter.patch('/experience/:eid', experience)
+_memberRouter.patch('/experience/:eid/end', experienceEnd)
+_memberRouter.patch('/experience/:eid/manifest', experienceManifest)
+_memberRouter.post('/', chat)
+_memberRouter.post('/bots', bots)
+_memberRouter.post('/bots/activate/:bid', activateBot)
+_memberRouter.post('/category', category)
+_memberRouter.post('contributions/:cid', contributions)
+_memberRouter.post('/mode', interfaceMode)
+_memberRouter.post('/upload', _upload)
+_memberRouter.put('/bots/:bid', bots)
+// Mount the subordinate routers along respective paths
+_Router.use('/members', _memberRouter.routes(), _memberRouter.allowedMethods())
+_Router.use('/api/v1', _apiRouter.routes(), _apiRouter.allowedMethods())
+/* mondular functions */
+/**
+ * Connects the routes to the router
+ * @param {object} _Menu Menu object
+ * @returns {object} Koa router object
+ */
+function connectRoutes(_Menu){
+	return _Router
+}
+/**
+ * Validates member session is locked
+ * @param {object} ctx Koa context object
+ * @param {function} next Koa next function
+ * @returns {function} Koa next function
+ */
+async function memberValidation(ctx, next) {
+    // validation logic
+    if(ctx.state.locked){
+        ctx.redirect(
+            ( ctx.params?.mid?.length??false)
+            ?   `/login/${encodeURIComponent(ctx.params.mid)}`
+            :   '/select'
+        ) // Redirect to /members if not authorized
+        return
+    }
+    await next() // Proceed to the next middleware if authorized
+}
+/**
+ * Returns the member session logged in status
+ * @param {object} ctx Koa context object
+ * @returns {boolean} false if member session is locked, true if registered and unlocked
+ */
+function status(ctx){	//	currently returns reverse "locked" status, could send object with more info
+	ctx.body = !ctx.state.locked
+}
+/**
+ * Returns the member session signup status
+ * @param {object} ctx Koa context object
+ * @returns {boolean} session user has signed up (t/f)
+ */
+function status_signup(ctx){
+	ctx.body = ctx.session.signup
+}
+// exports
+export default function init(_Menu) {
+	connectRoutes(_Menu)
+	return _Router
+}
