f58b9f0e740b233f411183efa201a27f1c3fbafa
Erik Jespersen <42016062+Mookse@users.noreply.github.com>
2024-05-08 13:37:26 -0400

    Initialize 160-openai-gpt-bots

---
+++ inc/js/core.mjs
@@ -0,0 +1,365 @@
+//	imports
+import EventEmitter from 'events'
+import chalk from 'chalk'
+//	import { _ } from 'ajv'
+//	server-specific imports
+import initRouter from './routes.mjs'
+import { _ } from 'ajv'
+//	define export Classes for Members and MyLife
+class Member extends EventEmitter {
+	#avatar
+	#categories = [
+		'Abilities',
+		'Artifacts',
+		'Beliefs',
+		'Biography',
+		'Challenges',
+		'Future',
+		'Goals',
+		'Health',
+		'Identity',
+		'Interests',
+		'Outlook',
+		'Personality',
+		'Preferences',
+		'Relationships',
+		'Sexuality',
+		'Updates'
+	]	//	base human categories [may separate into biological?ideological] of personal definitions for inc q's (per agent) for infusion
+	#factory	//	reference to session factory in all cases except for server/root MyLife/Q
+	constructor(_Factory){
+		super()
+		this.#factory = _Factory
+		/* assign factory/avatar listeners */
+		this.attachListeners()
+	}
+	/**
+	 * Initializes Member class instantiation and returns `this`
+	 * @async
+	 * @public
+	 * @returns {Promise} Promise resolves to this Member class instantiation
+	 */
+	async init(){
+		this.#avatar = await this.factory.getAvatar() // returns fully functional and initializaed avatar
+		return this
+	}
+	/**
+	 * Attaches listeners to Member class instantiation
+	 * @publc must remain public in order to be overridden
+	 * @returns {void} returns nothing
+	*/
+	attachListeners(){
+		this.factory.on('avatar-init-end',(_avatar,bytes)=>{
+			console.log(chalk.grey(`Member::init::avatar-init-end|memory-size=${bytes}b`))
+		})
+		this.factory.on('on-contribution-new',(_contribution)=>{
+			console.log(chalk.grey(`Member::on-contribution-new`),_contribution.request)
+		})
+	}
+	//	getter/setter functions
+	get abilities(){
+		return this.core.abilities
+	}
+	get agent(){
+		return this.#avatar
+	}
+	get agentCategories(){
+		return this.agent.categories
+	}
+	get agentCommand(){
+		return this.agent.command_word
+	}
+	get agentDescription(){	//	agent description (not required)
+		if(!this.agent?.description)
+			this.avatar.description = `I am ${ this.agentName }, AI-Agent for ${ this.name }`
+		return this.agent.description
+	}
+	get agentName(){
+		return this.avatar.names[0]
+	}
+	get agentProxy(){
+		switch(this.form){	//	need switch because I could not overload class function
+			case 'organization':
+				return this.description
+			default:
+				return this.bio
+		}
+	}
+	get avatar(){
+		return this.#avatar
+	}
+	set avatar(_Avatar){
+		//	oops, hack around how to get dna of avatar class; review options [could block at factory-getter level, most efficient and logical]
+		if(!this.factory.isAvatar(_Avatar))
+			throw new Error('avatar requires Avatar Class')
+		this.#avatar = _Avatar
+	}
+	/**
+	 * Gets being type. Members are _only_ human prototypes, and presents as human, even though the current manifestation is a datacore `core` being.
+	 * @returns {string} returns 'human'
+	*/
+	get being(){
+		return 'human'
+	}
+	get beliefs(){
+		return this.core.beliefs
+	}
+	get bio(){
+		return this.core.bio
+	}
+	get categories(){
+		return this.agent.categories
+	}
+	get chat(){
+		return this.agent.chat
+	}
+	get consent(){
+		return this.factory.consent	//	**caution**: returns <<PROMISE>>
+	}
+	set consent(_consent){
+		this.factory.consents.unshift(_consent.id)
+	}
+	/**
+	 * Gets Member Contributions, i.e., questions that need posing to Member
+	 * @returns {array} returns array of Member Contributions
+	 */
+	get contributions(){
+		return this.#avatar?.contributions
+	}
+	get core(){
+		return this.factory.core
+	}
+	get dataservice(){
+		return this.dataservices
+	}
+	get dataservices(){
+		return this.factory.dataservices
+	}
+	get description(){
+		return this.core.description
+	}
+	get email(){
+		return this.core.email
+	}
+	get factory(){
+		return this.#factory
+	}
+	get facts(){
+		return this.core.facts
+	}
+	get form(){
+		return this.core.form
+	}
+	get globals(){
+		return this.factory.globals
+	}
+	get hobbies(){
+		return this.core.hobbies
+	}
+	get interests(){
+		return this.core.interests
+	}
+	get id(){
+		return this.sysid
+	}
+	get mbr_id(){
+		return this.factory.mbr_id
+	}
+	get member(){
+		return this.core
+	}
+	get memberName(){
+		return this.core.names[0]
+	}
+	get name(){
+		return this.core.name
+	}
+	get newGuid(){
+		return this.globals.newGuid
+	}
+	get preferences(){
+		return this.core.preferences
+	}
+	get skills(){
+		return this.core.skills
+	}
+	get sysname(){
+		return this.mbr_id.split('|')[0]
+	}	
+	get sysid(){
+		return this.mbr_id.split('|')[1]
+	}
+	get values(){
+		return this.core.values
+	}
+	//	public functions
+	async testEmitters(){
+		//	test emitters with callbacks
+		this.emit('testEmitter',_response=>{
+			console.log('callback emitters enabled:',_response)
+		})
+	}
+}
+class Organization extends Member {	//	form=organization
+	#Menu
+	#Router
+	constructor(_Factory){
+		super(_Factory)
+	}
+	/* public functions */
+	async init(){
+		return await super.init()
+	}
+	/**
+	 * `Member` Overload
+	 * @publc must remain public in order to be overridden
+	 * @returns {void} returns nothing
+	*/
+	attachListeners(){
+		// intentionally empty
+	}
+	/* getters/setters */
+	/**
+	 * Gets being type. MyLife are _only_ MyLife prototypes, and presents as mylife, even though the current manifestation is a datacore `core` being.
+	 * @returns {string} returns 'MyLife'
+	*/
+	get being(){
+		return 'MyLife'
+	}
+	get description(){
+		return this.core.description
+	}
+	get governance(){
+		return this.core.governance
+	}
+	get membership(){
+		return this.core.membership
+	}
+	get menu(){
+		if(!this.#Menu){
+			this.#Menu = new (this.factory.schemas.menu)(this).menu
+		}
+		return this.#Menu
+	}
+	get mission(){
+		return this.core.mission
+	}
+	get name(){
+		return this.core.names[0]
+	}
+	get philosophy(){
+		return this.core.philosophy
+	}
+	get privacy(){
+		return this.core.privacy
+	}
+	get roadmap(){
+		return this.core.roadmap
+	}
+	get router(){
+		if(!this.#Router){
+			this.#Router = initRouter(new (this.factory.schemas.menu)(this))
+		}
+		return this.#Router
+	}
+	get security(){
+		return this.core.security
+	}
+	get services(){
+		return this.core.services
+	}
+	get values(){
+		return this.core.values
+	}
+	get vision(){
+		return this.core.vision
+	}
+}
+class MyLife extends Organization {	//	form=server
+	constructor(_Factory){	//	no session presumed to exist
+		super(_Factory)
+	}
+	async datacore(_mbr_id){
+		if(!_mbr_id || _mbr_id===this.mbr_id) throw new Error('datacore cannot be accessed')
+		return await this.factory.datacore(_mbr_id)
+	}
+	/* public functions */
+	/**
+	 * Server MyLife _Maht instantiation uses this function to populate the most current alerts in the modular factory memoryspace. Currently only applicable to system types, but since this is implemented at the `core.mjs` scope, we can account
+	 * @public
+	 * @returns {void} returns nothing, performs operation
+	 */
+	getAlerts(){
+		this.factory.getAlerts()
+	}
+	async getMyLifeSession(){
+		return await this.factory.getMyLifeSession()
+	}
+	/**
+	 * Registers a new candidate to MyLife membership
+	 * @public
+	 * @param {object} _candidate { 'email': string, 'humanName': string, 'avatarNickname': string }
+	 */
+	async registerCandidate(_candidate){
+		return await this.factory.registerCandidate(_candidate)
+	}
+	/**
+	 * Submits a story to MyLife via API. Unclear if can be dual-purposed for internal, or if internal still instantiates API context.
+	 * @public
+	 * @param {string} _mbr_id - Member id
+	 * @param {string} _assistantType - String name of assistant type
+	 * @param {string} _summary - String summary of story
+	 * @returns {object} - The story document from Cosmos.
+	 */
+	async story(_mbr_id, _assistantType, storySummary){
+		const id = this.globals.newGuid
+		const _story = {
+			assistantType: _assistantType,
+			being: 'story',
+			form: _assistantType,
+			id,
+			mbr_id: _mbr_id,
+			name: `${_assistantType}-story_${_mbr_id}_${id}`,
+			summary: storySummary,
+		}
+		const _storyCosmos = await this.factory.story(_story)
+		return this.globals.stripCosmosFields(_storyCosmos)
+	}
+	/**
+	 * Tests partition key for member
+	 * @public
+	 * @param {string} _mbr_id member id
+	 * @returns {boolean} returns true if partition key is valid
+	 */
+	async testPartitionKey(_mbr_id){
+		return await this.factory.testPartitionKey(_mbr_id)
+	}
+	/* getters/setters */
+	/**
+	 * Gets MyLife agent role, refers to server entity Maht/MyLife
+	 * @returns {object} returns { role: 'system', content: 'I am <||Q||>, AI-Agent for the nonprofit member organization MyLife' }
+	*/
+	get agentRole(){
+		switch(this.being){
+			default:	//	core
+				const replacedDescription = 'I am <||Q||>, AI-Agent for the nonprofit member organization MyLife'
+				return {
+						role: "system",
+						content: replacedDescription
+					}					
+		}
+	}
+	/**
+	 * Gets being type. MyLife are _only_ MyLife prototypes, and presents as mylife, even though the current manifestation is a datacore `core` being.
+	 * @returns {string} returns 'MyLife'
+	*/
+	get being(){
+		return 'MyLife'
+	}
+}
+/* exports */
+export {
+	Member,
+	Organization,
+	MyLife,
+}
---
+++ inc/js/mylife-data-service.js
@@ -0,0 +1,439 @@
+/**
+ * @fileOverview This file contains the Dataservices class, which manages data interactions for the MyLife platform.
+ * It includes functionality for handling avatars, chats, items, and other core elements of the platform's data.
+ * @version 1.0.0
+ */
+//	imports
+import { _ } from "ajv"
+import Datamanager from "./mylife-datamanager.mjs"
+import PgvectorManager from "./mylife-pgvector-datamanager.mjs"
+import { Guid } from "js-guid"
+/**
+ * The Dataservices class.
+ * This class provides methods to interact with the data layers of the MyLife platform, predominantly the Azure Cosmos and PostgreSQL database.
+ * Any new Dataservices class is instantiated with a member id, which is used to identify the member in the database, and retrieve the core data for that member.
+ */
+class Dataservices {
+    /**
+     * Represents the core functionality of the data service. This property
+     * objectifies core data to make it more manageable and structured,
+     * as opposed to presenting raw output.
+     * @private
+     */
+    #core
+    /**
+     * Manages various data-related operations. It could be responsible for
+     * handling data transactions, CRUD operations, etc., depending on its implementation.
+     * @private
+     */
+    #Datamanager
+    /**
+     * Identifies a specific partition or segment of the data storage
+     * that this instance of Dataservices interacts with. Useful for multi-tenant
+     * architectures or when data is sharded.
+     * @private
+     */
+    #partitionId
+    /**
+     * Manages interactions with Pgvector, which might be used for
+     * efficient vector operations in a Postgres database. This could include
+     * functionalities like similarity search, nearest neighbor search, etc.
+     * @private
+     */
+    #PgvectorManager
+    /**
+     * A default SQL SELECT statement or part of it used to fetch
+     * user-related data. It defines the columns to be retrieved in most
+     * database queries within this service.
+     * @private
+     */
+    #rootSelect = ['id', 'mbr_id', 'parent_id', 'being']
+    /**
+     * Constructor for Dataservices class.
+     * @param {string} _mbr_id - Member ID to partition data.
+     */
+	constructor(_mbr_id){
+		this.#partitionId = _mbr_id
+		this.#PgvectorManager = new PgvectorManager()
+	}
+    /**
+     * Initializes the Datamanager instance and sets up core data.
+     * @async
+	 * @public
+     * @returns {Dataservices} The instance of Dataservices.
+     */
+	async init(){
+		this.#Datamanager = new Datamanager(this.#partitionId)
+		await this.#Datamanager.init()	//	init datamanager
+		const _excludeProperties = { '_none':true }	//	populate if exclusions are required
+		const _core = Object.entries(this.datamanager.core)	//	array of arrays
+			.filter((_prop)=>{	//	filter out excluded properties
+				const _charExlusions = ['_','@','$','%','!','*',' ']
+				return !(
+						(_prop[0] in _excludeProperties)
+					||	!(_charExlusions.indexOf(_prop[0].charAt()))
+				)
+				})
+			.map(_prop=>{	//	map to object
+				return { [_prop[0]]:_prop[1] }
+			})
+		this.#core = Object.assign({},..._core)	//	init core
+		return this
+	}
+	//	getters/setters
+	get core(){
+		return this.#core
+	}
+	get datamanager(){
+		return this.#Datamanager
+	}
+	get embedder(){
+		return this.#PgvectorManager
+	}
+	get globals(){
+		return this.datamanager.globals
+	}
+	get id(){
+		return this.partitionId.split('|')[1]
+	}
+	get isMyLife(){
+		return this.mbr_id===process.env?.MYLIFE_SERVER_MBR_ID??false
+	}
+	get mbr_id(){
+		return this.partitionId
+	}
+	get partitionId(){
+		return this.#partitionId
+	}
+	//	public functions
+	/**
+	 * Get a bot.
+	 * @public
+	 * @param {string} _bot_id - The bot id.
+	 * @returns {object} - The bot.
+	 */
+	async bot(_bot_id, _bot_type='personal-avatar'){
+		if(_bot_id){
+			return await this.getItem(_bot_id)
+		} else {
+			const _bots = await this.bots(_bot_type)
+			return _bots?.[0]
+		}
+	}
+	async bots(_bot_type){
+		if(_bot_type){
+			return await this.getItems(
+				'bot',
+				['bot_id'],
+				[{ name: '@bot_type', value: _bot_type }],
+			)
+		} else {
+			return await this.getItems('bot')
+		}
+	}
+	async botInstructions(_type){
+		return await this.getItems(
+			'bot-instructions',
+			undefined,
+			[{ name: '@type', value: _type }],
+			'system'
+		)
+	}
+    /**
+     * Challenges access using a member ID and passphrase.
+     * @async
+	 * @public
+     * @param {string} _mbr_id - The member ID.
+     * @param {string} _passphrase - The passphrase for access.
+     * @returns {Promise<Object>} The result of the access challenge.
+     */
+	async challengeAccess(_mbr_id,_passphrase){	//	if possible (async) injected into session object
+		//	ask global data service (stored proc) for passphrase
+		return await this.datamanager.challengeAccess(_mbr_id, _passphrase)
+	}
+	async datacore(_mbr_id){
+		return await this.getItem(_mbr_id)
+	}
+	async findRegistrationIdByEmail(_email){
+		/* pull record for email, returning id or new guid */
+		const _ = await this.getItems(
+			'registration',
+			undefined,
+			[{ name: '@email',
+				value: _email,
+			}],
+			'registration',
+		)
+		return _?.[0]?.id??Guid.newGuid().toString() // needed to separate out, was failing
+	}
+	/**
+	 * Retrieves a specific alert by its ID. _Currently placehoder_.
+	 * @async
+	 * @public
+	 * @param {string} _alert_id - The unique identifier for the alert.
+	 * @returns {Promise<Object>} The alert corresponding to the provided ID.
+	 */
+	async getAlert(_alert_id){
+		return await this.getItem(_alert_id, 'system')
+	}
+	/**
+	 * Retrieves all system alerts. _Currently only works with system alerts, but intends to be expanded, refactor_.
+	 * This method is typically used to get all alert entities under a specific object.
+	 * @async
+	 * @public
+	 * @param {string} _object_id - The parent object ID to search for associated alerts.
+	 * @returns {Promise<Array>} An array of alerts associated with the given parent ID.
+	 */
+	async getAlerts(){	
+		const _paramsArray = [
+			{ name: '@being', value: 'alert' },
+			{ name: '@currentDate', value: new Date().toISOString() }
+		]
+		const _query = `SELECT * FROM c WHERE c.being = @being AND @currentDate >= c.timestampRange['start'] AND @currentDate <= c.timestampRange['end']`
+
+		return await this.datamanager.getItems(
+			{ query: _query, parameters: _paramsArray },
+			'system',
+		)
+	}
+	async getAvatar(){
+		const _avatars = await this.getAvatars()
+		return _avatars?.[0]
+	}
+	async getAvatars(){
+		return await this.getItems('avatar')
+	}
+	/**
+	 * Retrieves the first chat associated with a given parent ID.
+	 * If multiple chats are associated with the parent ID, only the first one is returned.
+	 * @async
+	 * @public
+	 * @param {string} [parent_id=this.id] - The parent ID for which to retrieve the chat. Defaults to the current instance ID.
+	 * @returns {Promise<Object>} The first chat object associated with the given parent ID.
+	 */
+	async getChat(parent_id = this.id) {
+		const _response = await this.getChats(parent_id)
+		return _response[0] // Extract the first chat from the response
+	}
+	/**
+	 * Retrieves all chat conversations associated with a given parent ID.
+	 * If no chats exist, it creates a new chat conversation.
+	 * @async
+	 * @public
+	 * @param {string} parent_id - The parent ID for which to retrieve or create chats.
+	 * @returns {Promise<Array>} An array of chat conversations associated with the given parent ID.
+	 */
+	async getChats(parent_id) {
+		let _chats = await this.getItems(
+			'conversation',
+			undefined,
+			[{ name: '@parent_id', value: parent_id }],
+		)
+		if (!_chats.length) {
+			_chats = await this.pushItem({
+				mbr_id: this.mbr_id,
+				parent_id: parent_id,
+				being: 'conversation',
+				exchanges: [],
+				name: `conversation_${this.mbr_id}`,
+			})
+		}
+		return _chats
+	}
+	/**
+	 * Retrieves all seed contribution questions associated with being & category.
+	 * @async
+	 * @public
+	 * @param {string} _being - The type of underlying datacore.
+	 * @param {string} _category - The category of seed questions to retrieve.
+	 * @returns {Promise<Object>} The item corresponding to the provided ID.
+	 */
+	async getContributionQuestions(_being, _category, _maxNumber=3){
+		return (await this.getItems(
+			_being,
+			['questions'],
+			[{ name: '@category', value: _category }],
+			'contribution_responses',
+		))
+			.map(_ => (_.questions))
+			.reduce((acc, val) => acc.concat(val), [])
+			.sort(() => Math.random() - Math.random())
+			.slice(0, _maxNumber)
+	}
+	/**
+	 * Retrieves a specific item by its ID.
+	 * @async
+	 * @public
+	 * @param {string} _id - The unique identifier for the item.
+	 * @param {string} _container_id - The container to use, overriding default: `Members`.
+	 * @param {string} _mbr_id - The member id to use, overriding default.
+	 * @returns {Promise<Object>} The item corresponding to the provided ID.
+	 */
+	async getItem(_id, _container_id, _mbr_id=this.mbr_id) {
+		try{
+			return await this.datamanager.getItem(
+				_id,
+				_container_id,
+				{ partitionKey: _mbr_id, populateQuotaInfo: false, },
+			)
+		}
+		catch(_error){
+			console.log('mylife-data-service::getItem() error')
+			console.log(_error, _id, _container_id,)
+		}
+	}
+	/**
+	 * Retrieves items based on specified parameters.
+	 * @async
+	 * @public
+	 * @param {string} _being - The type of items to retrieve.
+	 * @param {array} [_selects=[]] - Fields to select; if empty, selects all fields.
+	 * @param {Array<Object>} [_paramsArray=[]] - Additional query parameters.
+	 * @param {string} _container_id - The container name to use, overriding default.
+	 * @param {string} _mbr_id - The member id to use, overriding default.
+	 * @returns {Promise<Array>} An array of items matching the query parameters.
+	 */
+	async getItems(_being, _selects=[], _paramsArray=[], _container_id, _mbr_id=this.mbr_id) {	//	_params is array of objects { name: '${varName}' }
+		// @todo: incorporate date range functionality into this.getItems()
+		const _prefix = 'u'
+		_paramsArray.unshift({ name: '@being', value: _being })	//	add primary parameter to array at beginning
+		const _selectFields = (_selects.length)
+			?	[...this.#rootSelect, ..._selects].map(_=>(`${_prefix}.`+_)).join(',')
+			:	'*'
+		let _query = `select ${_selectFields} from ${_prefix}`	//	@being is required
+		_paramsArray	//	iterate through parameters
+			.forEach(_param=>{	//	param is an object of name, value pairs
+				_query += (_param.name==='@being')
+					?	` where ${_prefix}.${_param.name.split('@')[1]}=${_param.name}`	//	only manages string so far
+					:	` and ${_prefix}.${_param.name.split('@')[1]}=${_param.name}`	//	only manages string so far
+		})
+		try{
+			return await this.datamanager.getItems(
+				{ query: _query, parameters: _paramsArray },
+				_container_id,
+				{
+					partitionKey: _mbr_id,
+					populateQuotaInfo: false, // set this to true to include quota information in the response headers
+				},
+			)
+		}
+		catch(_error){
+			console.log('mylife-data-service::getItems() error')
+			console.log(_error, _being, _query, _paramsArray, _container_id,)
+		}
+	}
+	/**
+	 * Retrieves local records based on a query.
+	 * @async
+	 * @param {string} _question - The query to retrieve records.
+	 * @returns {Promise<Array>} An array of local records matching the query.
+	 */
+	async getLocalRecords(_question){
+		return await this.embedder.getLocalRecords(_question)
+	}
+	/**
+	 * Patches an item by its ID with the provided data.
+	 * @async
+	 * @param {string} _id - The unique identifier for the item to be patched.
+	 * @param {Object} _data - The data to patch the item with.
+	 * @param {string} [_path='/'] - The path for patching, defaults to root.
+	 * @returns {Promise<Object>} The result of the patch operation.
+	 */
+	async patch(_id, _data, _path = '/') {
+		// @todo: limit to 10 patch operations
+		// _data is an object of key/value pairs to be transformed into patch operations
+		const patchOperations = Object.keys(_data)
+			// Filtering out keys that should not be included in the patch
+			.filter(_key => !['id', 'being', 'mbr_id'].includes(_key))
+			.map(_key => {
+				return { op: 'replace', path: _path + _key, value: _data[_key] };
+			})
+		// Performing the patch operation
+		return await this.patchItem(_id, patchOperations);
+	}	
+	/**
+	 * Patches an array within an item by inserting data at a specified index.
+	 * @async
+	 * @param {string} _id - The unique identifier for the item to be patched.
+	 * @param {string} _node - The node within the item where data is to be inserted.
+	 * @param {Array<Object>} _data - The data to be inserted.
+	 * @param {number} [_index=0] - The index at which to insert the data.
+	 * @returns {Promise<Object>} The result of the patch operation.
+	 */
+	async patchArrayItems(_id,_node,_data){	//	_data is array of objects to be inserted into _node at _index
+		// changed to hard overwrite as the unshift was not working
+		// @todo: remove index
+		const __data = [{ op: 'replace', path: `/${_node}`, value: _data }]
+		return await this.patchItem(_id,__data)
+	}
+	/**
+	 * Patches an item with the given data. The path for each patch operation is embedded in the data.
+	 * @async
+	 * @param {string} _id - The unique identifier for the item to be patched.
+	 * @param {Array<Object>} _data - The data for patching, including the path and operation.
+	 * @returns {Promise<Object>} The result of the patch operation.
+	 */
+	async patchItem(_id,_data){ // path Embedded in _data
+		return await this.datamanager.patchItem(_id,_data)
+	}
+    /**
+     * Pushes a new item to the data manager
+     * @async
+	 * @public
+     * @param {Object} _data - The data to be pushed
+     * @returns {Promise<Object>} The result of the push operation
+     */
+	async pushItem(_data){
+		return await this.datamanager.pushItem(_data)
+	}
+	/**
+	 * Registers a new candidate to MyLife membership after finding record (or contriving Guid) in db
+	 * @public
+	 * @param {object} _candidate { 'email': string, 'humanName': string, 'avatarNickname': string }
+	 */
+	async registerCandidate(_candidate){
+		_candidate.mbr_id = this.#partitionId
+		_candidate.id = await this.findRegistrationIdByEmail(_candidate.email)
+		_candidate.being = 'registration'
+		_candidate.name = `${_candidate.email.split('@')[0]}-${_candidate.email.split('@')[1]}_${_candidate.id}`
+		return await this.datamanager.registerCandidate(_candidate)
+	}
+	async setBot(_bot){
+		const _originalBot = await this.bot(_bot.id)
+		if(_originalBot){ // update
+			const _changed = Object.keys(_bot)
+				.filter(key => !key.startsWith('_') && _bot[key] !== _originalBot[key])
+				.reduce((obj, key) => {
+					obj[key] = _bot[key]
+					return obj
+				}, {})
+			if (Object.keys(_changed).length > 0) {
+				this.patch(_bot.id, _changed)
+			}
+		} else { // add
+			this.pushItem(_bot)
+		}
+		return _bot
+	}
+	/**
+	 * Submits a story to MyLife. Currently via API, but could be also work internally.
+	 * @param {object} _story - Story object { assistantType, being, form, id, mbr_id, name, summary }.
+	 * @returns {object} - The story document from Cosmos.
+	 */
+	async story(_story){
+		if(!this.isMyLife) return
+		return await this.datamanager.pushItem(_story)
+	}
+	/**
+	 * Tests partition key for member
+	 * @public
+	 * @param {string} _mbr_id member id
+	 * @returns {boolean} returns true if partition key is valid
+	 */
+	async testPartitionKey(_mbr_id){
+		if(!this.isMyLife) return false
+		return await this.datamanager.testPartitionKey(_mbr_id)
+	}
+}
+//	exports
+export default Dataservices
