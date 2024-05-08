ad36760cc7b0ea99b0c9d25b2d7a5ab84557fcc4
Erik Jespersen <42016062+Mookse@users.noreply.github.com>
2024-05-08 13:37:31 -0400

    Initialize 184-keyvalidation

---
+++ inc/js/factory-class-extenders/class-extenders.mjs
@@ -0,0 +1,442 @@
+import { EventEmitter } from 'events'
+import {
+    mGetQuestions,
+    mUpdateContribution,
+} from './class-contribution-functions.mjs'
+import {
+    mSaveConversation,
+} from './class-conversation-functions.mjs'
+import{
+    mAppear,
+    mDialog,
+    mGetEvent,
+    mInput,
+    mGetScene,
+    mGetSceneNext,
+} from './class-experience-functions.mjs'
+import {
+	mAssignContent,
+} from './class-message-functions.mjs'
+/**
+ * Extends the `Consent` class.
+ * @todo - global conversion of parent_id -> object_id
+ * @param {*} originClass - The class to extend.
+ * @param {Object} referencesObject - The references to extend the class with, factory, llm, etc.
+ * @returns {Consent} - The extended class definition.
+ */
+function extendClass_consent(originClass, referencesObject) {
+    class Consent extends originClass {
+        constructor(obj) {
+            super(obj)
+        }
+        //  public functions
+        async allow(_request){
+            //	this intends to evolve in near future, but is currently only a pass-through with some basic structure alluding to future functionality
+            return true
+        }
+    }
+    return Consent
+}
+/**
+ * Extends the `Contribution` class.
+ * @param {*} originClass - The class to extend.
+ * @param {Object} referencesObject - The references to extend the class with, factory, llm, etc.
+ * @returns {Contribution} - The `Contribution` extended class definition.
+ */
+function extendClass_contribution(originClass, referencesObject) {
+    class Contribution extends originClass {
+        #emitter = new EventEmitter()
+        #factory
+        #llm = referencesObject?.openai
+        constructor(_obj) {
+            super(_obj)
+        }
+        /* public functions */
+        /**
+         * Initialize a contribution.
+         * @async
+         * @public
+         * @param {object} _factory - The factory instance.
+         */
+        async init(_factory){
+            this.#factory = _factory
+            this.request.questions = await mGetQuestions(this, this.#llm) // generate question(s) from cosmos or openAI
+            this.id = this.factory.newGuid
+            this.status = 'prepared'
+            this.emit('on-contribution-new',this)
+            return this
+        }
+        async allow(_request){
+            //	todo: evolve in near future, but is currently only a pass-through with some basic structure alluding to future functionality
+            return true
+        }
+        /**
+         * Convenience proxy for emitter.
+         * @param {string} _eventName - The event to emit.
+         * @param {any} - The event(s) to emit.
+         * @returns {void}
+         */
+        emit(_eventName, ...args){
+            this.#emitter.emit(_eventName, ...args)
+        }
+        /**
+         * Convenience proxy for listener.
+         * @param {string} _eventName - The event to emit.
+         * @param {function} _listener - The event listener functionality.
+         * @returns {void}
+         */
+        on(_eventName, _listener){
+            this.#emitter.on(_eventName, _listener)
+        }
+        /**
+         * Updates `this` with incoming contribution data.
+         * @param {object} _contribution - Contribution data to incorporate 
+         * @returns {void}
+         */
+        update(_contribution){
+            mUpdateContribution(this, _contribution) // directly modifies `this`, no return
+        }
+        /*  getters/setters */
+        /**
+         * Get the factory instance.
+         * @returns {object} MyLife Factory instance
+         */
+        get factory(){
+            return this.#factory
+        }
+        get memberView(){
+            return this.inspect(true)
+        }
+        get openai(){
+            return this.#llm
+        }
+        get questions(){
+            return this?.questions??[]
+        }
+        /* private functions */
+    }
+    return Contribution
+}
+/**
+ * Extends the `Conversation` class.
+ * @param {*} originClass - The class to extend.
+ * @param {Object} referencesObject - The references to extend the class with, factory, llm, etc.
+ * @returns {Conversation} - The `Conversation` extended class definition.
+ */
+function extendClass_conversation(originClass, referencesObject) {
+    class Conversation extends originClass {
+        #botId
+        #factory
+        #messages = []
+        #saved = false
+        #thread
+        /**
+         * 
+         * @param {Object} obj - The object to construct the conversation from.
+         * @param {AgentFactory} factory - The factory instance.
+         * @param {Object} thread - The thread instance.
+         * @param {Guid} botId - The initial active bot id (can mutate)
+         */
+        constructor(obj, factory, thread, botId) {
+            super(obj)
+            this.#factory = factory
+            this.#thread = thread
+            this.#botId = botId
+            this.name = `conversation_${this.#factory.mbr_id}_${thread.thread_id}`
+            this.type = this.type??'chat'
+        }
+        /* public functions */
+        /**
+         * Adds a `Message` instances to the conversation.
+         * @public
+         * @param {Object|Message} message - Message instance or object data to add.
+         * @returns {Object[]} - The updated messages array.
+         */
+        addMessage(message){
+            if(this.messages.find(_message=>_message.id===message.id))
+                return this.messages
+            if(!(message instanceof this.#factory.message)){
+                if(typeof message!=='object')
+                    message = { content: message }
+                message = new (this.#factory.message)(message)
+            }
+            this.#messages = [message, ...this.messages]
+            return this.messages
+        }
+        /**
+         * Adds an array of `Message` instances to the conversation.
+         * @public
+         * @param {Object[]} messages - Array of messages to add.
+         * @returns {Object[]} - The updated messages array.
+         */
+        addMessages(messages){
+            messages
+                .sort((mA, mB) => mA.created_at - mB.created_at)
+                .forEach(message => this.addMessage(message))
+            return this.messages
+        }
+        /**
+         * Get the message by id, or defaults to last message added.
+         * @public
+         * @param {Guid} messageId - The message id.
+         * @returns {object} - The openai `message` object.
+         */
+        async getMessage(messageId){
+            return messageId && this.messages?.[0]?.id!==messageId
+                ? this.messages.find(message=>message.id===messageId)
+                : this.message
+        }
+        async save(){
+            // also if this not saved yet, save to cosmos
+            if(!this.isSaved){
+                await mSaveConversation(this.#factory, this)
+            }
+            //  save messages to cosmos
+            // @todo: no need to await
+            await this.#factory.dataservices.patch(
+                this.id,
+                { messages: this.messages.map(_msg=>_msg.micro), }
+            )
+            // flag as saved
+            this.#saved = true
+            return this
+        }
+        //  public getters/setters
+        get bot_id(){
+            return this.#botId
+        }
+        get botId(){
+            return this.bot_id
+        }
+        set botId(_botId){
+            this.#botId = _botId
+        }
+        get isSaved(){
+            return this.#saved
+        }
+        /**
+         * Get the most recently added message.
+         * @getter
+         * @returns {Message} - The most recent message.
+         */
+        get message(){
+            return this.messages[0]
+        }
+        get messages(){
+            return this.#messages
+        }
+        /**
+         * Gets most recent dialog contribution to conversation.
+         * @getter
+         * @returns {object} - Most recent facet of dialog from conversation.
+         */
+        get mostRecentDialog(){
+            return this.message.content
+        }
+        get thread(){
+            return this.#thread
+        }
+        get thread_id(){
+            return this.threadId
+        }
+        get threadId(){
+            return this.thread.id
+        }
+    }
+    return Conversation
+}
+/**
+ * Extends the `Experience` class.
+ * @param {*} originClass - The class to extend.
+ * @param {Object} referencesObject - The references to extend the class with, factory, llm, etc.
+ * @returns {Experience} - The `Experience` extended class definition.
+ */
+function extendClass_experience(originClass, referencesObject){
+    class Experience extends originClass {
+        #cast = []
+        constructor(obj) {
+            super(obj)
+        }
+        /* public functions */
+        /**
+         * Initialize the experience.
+         * @todo - implement building classes either on-demand or on-init to create scene and event classes
+         * @returns {Experience} - The initialized experience.
+         */
+        init(){
+            /* self-validation */
+            if(!this.scenes || !this.scenes.length)
+                throw new Error('No scenes provided for experience')
+            /* sort scenes/events by order in place */
+            this.scenes.sort((_a, _b)=>(_a?.order??0)-(_b.order??0))
+            this.scenes.forEach(_scene=>{
+                if(!_scene.events || !_scene.events.length)
+                    throw new Error('No events provided for scene')
+                _scene.events.sort((_a, _b)=>(_a?.order??0)-(_b.order??0))
+            })
+            return this
+        }
+        /**
+         * From specified event, returns `synthetic` Dialog data package, see `mDialog` in `class-experience-functions.mjs`.
+         * @param {Guid} eventId - The event id.
+         * @param {number} iteration - The iteration number, array-variant.
+         * @returns {object} - `synthetic` Dialog data package.
+         */
+        dialogData(eventId, iteration=0){
+            return mDialog(this.event(eventId), iteration)
+        }
+        /**
+         * Gets a specified event from the experience. Throws error if not found.
+         * @param {Array} scenes - The array of scenes to search.
+         * @param {Guid} eventId - The event id.
+         * @returns {object} - The event object data.
+         * @throws {Error} - If event not found.
+         */
+        event(eventId){
+            return mGetEvent(this.scenes, eventId)
+        }
+        /**
+         * From specified event, returns `synthetic` Input data package, see `mInput` in `class-experience-functions.mjs`.
+         * @param {Guid} eventId - The event id.
+         * @param {number} iteration - The iteration number, array-variant.
+         * @returns {object} - `synthetic` Input data package.
+         */
+        input(eventId, iteration=0){
+            return mInput(this.event(eventId), iteration)
+        }
+        /**
+         * Gets a specified scene from the experience. Throws error if not found.
+         * @param {Guid} sceneId 
+         * @returns {object} - The scene object data.
+         */
+        scene(sceneId){
+            return mGetScene(this.scenes, sceneId)
+        }
+        sceneNext(sceneId){
+            return mGetSceneNext(this.scenes, sceneId)
+        }
+        /* getters/setters */
+        /**
+         * Get the cast of the experience.
+         * @getter
+         * @returns {ExperienceCastMember[]} - The array of cast members.
+         */
+        get castMembers(){
+            return this.cast.map(castMember=>{
+                const { bot_id, icon, id, name, role, type, url, } = castMember
+                return { bot_id, icon, id, name, role, type, url, }
+            })
+        }
+        /**
+         * Get the experience in frontend format. Currently intentionally omitting manifest, grab separately, they are not "required". Scenes and events are only required in `eventSequences`.
+         * @getter
+         * @returns {object} - The `synthetic` experience object.
+         */
+        get experience(){
+            const { autoplay, description, goal, id, location, purpose, skippable, title, version } = this
+            return {
+                autoplay,
+                description,
+                goal,
+                id,
+                location,
+                purpose,
+                skippable,
+                title,
+                version: version ?? 0,
+            }
+        }
+        /**
+         * Get the manifest of the experience.
+         * @getter
+         * @returns {object} - The manifest of the experience.
+         * @property {array} cast - The cast array of the experience.
+         * @property {object} navigation - The navigation object of the experience.
+         */
+        get manifest(){
+            return {
+                cast: this.castMembers,
+                navigation: this.navigation,
+            }
+        }
+    }
+    return Experience
+}
+/**
+ * Extends the `File` class.
+ * @param {*} originClass - The class to extend.
+ * @param {Object} referencesObject - The references to extend the class with, factory, llm, etc.
+ * @returns {File} - The `File` extended class definition.
+ */
+function extendClass_file(originClass, referencesObject) {
+    class File extends originClass {
+        #contents   //  utilized _only_ for text files
+        constructor(_obj) {
+            super(_obj)
+        }
+        //  public functions
+        async init(){
+            //  self-validation
+            if(!this.contents && this.type=='text')
+                throw new Error('No contents provided for text file; will not store')
+            //  save to embedder
+        }
+        //  public getters/setters
+        //  private functions
+    }
+    return File
+}
+/**
+ * Extends the `Message` class.
+ * @param {*} originClass - The class to extend.
+ * @param {Object} referencesObject - The references to extend the class with, factory, llm, etc.
+ * @returns {Message} - The `Message` extended class definition.
+ */
+function extendClass_message(originClass, referencesObject) {
+    /**
+     * Message class.
+     * @class
+     * @extends originClass - variable that defines the _actual_ class to extend, here message.
+     * @param {object} obj - The object to construct the message from..
+     */
+    class Message extends originClass {
+        #content
+        constructor(obj) {
+            const { content, ..._obj } = obj
+            super(_obj)
+            try{
+                this.#content = mAssignContent(content??obj)
+            } catch(e){
+                console.log('Message::constructor::ERROR', e)
+                this.#content = ''
+            }
+        }
+        /* getters/setters */
+        get content(){
+            return this.#content
+        }
+        set content(_content){
+            try{
+                this.#content = mAssignContent(_content)
+            } catch(e){
+                console.log('Message::content::ERROR', e)
+            }
+        }
+        get message(){
+            return this
+        }
+        get micro(){
+            return { content: this.content, role: this.role??'user' }
+        }
+    }
+    return Message
+}
+/* exports */
+export {
+	extendClass_consent,
+    extendClass_contribution,
+    extendClass_conversation,
+    extendClass_experience,
+    extendClass_file,
+	extendClass_message,
+}
---
+++ inc/js/session.mjs
@@ -0,0 +1,202 @@
+import { EventEmitter } from 'events'
+import chalk from 'chalk'
+class MylifeMemberSession extends EventEmitter {
+	#alertsShown = [] // array of alert_id's shown to member in this session
+	#autoplayed = false // flag for autoplayed experience, one per session
+	#consents = []	//	consents are stored in the session
+	#contributions = []	//	intended to hold all relevant contribution questions for session
+	#experiences = []	//	holds id for experiences conducted in this session
+	#factory
+	#locked = true	//	locked by default
+	#mbr_id = false
+	#Member
+	name
+	constructor(_factory){
+		super()
+		this.#factory = _factory
+		this.#mbr_id = this.isMyLife ? this.factory.mbr_id : false
+		mAssignFactoryListeners(this.#factory)
+		console.log(
+			chalk.bgGray('MylifeMemberSession:constructor(_factory):generic-mbr_id::end'),
+			chalk.bgYellowBright(this.factory.mbr_id),
+		)
+	}
+	async init(mbr_id=this.mbr_id){
+		if(this.locked){
+			if(this.#Member?.mbr_id??mbr_id !== mbr_id)
+				console.log(chalk.bgRed('cannot initialize, member locked'))
+		}
+		if(this.mbr_id && this.mbr_id !== mbr_id) { // unlocked, initialize member session
+			this.#mbr_id = mbr_id
+			mAssignFactoryListeners(this.#factory)
+			await this.#factory.init(this.mbr_id, )	//	needs only `init()` with different `mbr_id` to reset
+			this.#Member = await this.factory.getMyLifeMember()
+			this.#autoplayed = false // resets autoplayed flag, although should be impossible as only other "variant" requires guest status, as one day experiences can be run for guests also [for pay]
+			this.emit('onInit-member-initialize', this.#Member.memberName)
+			console.log(
+				chalk.bgBlue('created-member:'),
+				chalk.bgRedBright(this.#Member.memberName)
+			)
+		}
+		return this
+	}
+	async alert(_alert_id){
+		return this.factory.getAlert(_alert_id)
+	}
+	async alerts(_type){
+		let currentAlerts = this.factory.alerts
+		// remove alerts already shown to member in this session
+		currentAlerts = currentAlerts
+			.filter(_alert=>{
+				return !this.#alertsShown.includes(_alert.id)
+			})
+		currentAlerts.forEach(_alert=>{
+			this.#alertsShown.push(_alert.id)
+		})
+		return currentAlerts
+	}
+	async challengeAccess(_passphrase){
+		if(this.locked){
+			if(!this.challenge_id) return false	//	this.challenge_id imposed by :mid from route
+			if(!await this.factory.challengeAccess(this.challenge_id, _passphrase)) return false	//	invalid passphrase, no access [converted in this build to local factory as it now has access to global datamanager to which it can pass the challenge request]
+			//	init member
+			this.#locked = false
+			this.emit('member-unlocked', this.challenge_id)
+			await this.init(this.challenge_id)
+		}
+		return !this.locked
+	}
+	/**
+	 * Gets experiences for the member session. Identifies if autoplay is required, and if so, begins avatar experience via this.#Member. Ergo, Session can request avatar directly from Member, which was generated on `.init()`.
+	 * @todo - get and compare list of lived-experiences
+	 * @param {boolean} includeLived - Include lived experiences in the list.
+	 * @returns {Promise<object>} - Experiences shorthand: { autoplay: guid??false, events: [], experiences: [] }
+	 * @property {Guid} autoplay - Autoplay experience id.
+	 * @property {Object[]} events - Array of events.
+	 * @property {Object[]} experiences - Array of experiences.
+	 */
+	async experiences(includeLived=false){
+		if(this.locked) return {}
+		const { avatar } = this.#Member
+		const experiences = await avatar.experiences(includeLived)
+		let autoplay = experiences.find(experience=>experience.autoplay)?.id
+			?? false
+		/* trigger auto-play from session */
+		if(!this.#autoplayed && this.globals.isValidGuid(autoplay)){
+            const _start = await avatar.experienceStart(autoplay)
+			this.#autoplayed = true
+        }
+		else
+			autoplay = false
+		return { autoplay, experiences, mbr_id: this.mbr_id }
+	}
+	//	consent functionality
+	async requestConsent(ctx){
+		//	validate request; switch true may be required
+		if(!mValidCtxObject(ctx)) return false	//	invalid ctx object, consent request fails
+		//	check-01: url ends in valid guid /:_id
+		const _object_id = ctx.request.header?.referer?.split('/').pop()
+		//	not guid, not consent request, no blocking
+		if(!this.globals.isValidGuid(_object_id)) return true
+		console.log('session.requestConsent()', 'mbr_id', this.mbr_id)
+		//	ultimately, applying a disposable agent of intelligence to consent request might be the answer
+		let _consent = this.consents
+			.filter(_=>{ return _.id==_object_id })
+			.pop()
+		if(!_consent){
+			//	create and notify session
+			_consent = await this.factory.getConsent({
+				id: _object_id,
+				mbr_id: this.mbr_id,
+				being: 'consent',
+				context: `This consent object was created by MyLife Session [${this.mbr_id}] to manage access to asset [${_object_id}] of owner [${this.mbr_id}].`,
+				purpose: `To manage requested access to the underlying avatar or object according to the wills and expressions of the member identified by their mbr_id: ${this.mbr_id}.`,
+			})
+			this.consents.unshift(_consent)
+		}
+		return _consent.allow()	//	might benefit from putting consent into openai assistant metadata with suggestion to adhere when creating content
+		if(!ctx.request.body) return	//	nothing to do
+		//	based on incoming request, parse out consent id and request
+		return this.factory.requestConsent(_consent_id,_request)
+		
+		if(!this?.ctx?.session?.MemberSession?.consents) return	//	pre-natal instance of MyLife?
+		//	otherwise, should be able to construe
+		_consent = (_consent_id)
+			?	{}	//	retrieve from Cosmos
+			:	new (this.schemas.consent)(_request, this)	//	generate new consent
+		console.log('_consent', _consent)
+		//	manipulate session through ctx (although won't exist in initial test case)
+		await (this.ctx.session.MemberSession.consents = _consent)	//	will add consent to session list
+		return _consent
+	}
+	get consent(){
+		return this.factory.consent	//	**caution**: returns <<PROMISE>>
+	}
+	set consent(_consent){
+		this.#consents.unshift(_consent)
+	}
+	get consents(){
+		return this.#consents
+	}
+	get contributions(){
+		return this.#contributions
+	}
+	get core(){
+		return this.factory.core
+	}
+	get factory(){
+		return this.#factory
+	}
+	get globals(){
+		return this.factory.globals
+	}
+	get isInitialized(){
+		return ( this.mbr_id!==false )
+	}
+	get isMyLife(){
+		return this.factory.isMyLife
+	}
+	get locked(){
+		return this.#locked
+	}
+	get mbr_id(){
+		return this.#mbr_id
+	}
+	get mbr_id_id(){
+		return this.globals.sysId( this.mbr_id )
+	}
+	get member(){ // @todo - deprecate and funnel through any requests
+		return this.#Member
+	}
+	get subtitle(){
+		return this.#Member?.agentName
+	}
+}
+function mAssignFactoryListeners(_session){
+	if(_session.isMyLife) // all sessions _begin_ as MyLife
+		_session.factory.on('member-unlocked',_mbr_id=>{
+			console.log(
+				chalk.grey('session::constructor::member-unlocked_trigger'),
+				chalk.bgGray(_mbr_id)
+			)
+		})
+	else // all _"end"_ as member
+		_session.factory.on('avatar-activated',_avatar=>{
+			console.log(
+				chalk.grey('session::constructor::avatar-activated_trigger'),
+				chalk.bgGray(_avatar.id)
+			)
+		})
+}
+function mValidCtxObject(_ctx){
+	//	validate ctx object
+	return	(
+			_ctx
+		&&	typeof _ctx === 'object'
+		&&	'request' in _ctx 
+		&&	'response' in _ctx
+		&&	'session' in _ctx
+		&&	'state' in _ctx
+	)
+}
+export default MylifeMemberSession
